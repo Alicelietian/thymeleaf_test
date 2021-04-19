26adc2cfdd2c7e2b3ff704e976ef25179a28ce44
wenjiajun <1098926887@qq.com>
2021-04-19 21:17:20 +0800

    编写了个主函数

---
+++ src/main/java/com/offcn/controller/TestController01.java
@@ -0,0 +1,12 @@
+package com.offcn.controller;
+
+/**
+ * @author a1098
+ * @date 2021/4/19 21:15
+ * @Description
+ */
+public class TestController01 {
+    public static void main(String[] args) {
+        System.out.println("测试提交");
+    }
+}
